107aa11e165b23883f734e027d95e5c6fced4e32
gitRaiku <sebi.pintilie@gmail.com>
2024-01-21 18:12:56 +0200

    Da, da, DA.

--- shaders/cloud_frag.glsl
+++ shaders/cloud_frag.glsl
@@ -12,35 +12,35 @@ uniform sampler3D tex;
 
 vec3 fix(vec3 p) { return vec3(p + vec3(1.0)) / 2.0; }
 
-float p2(float x) { return x * x; }
-
 #define PI 3.1415
 
 vec3 dist(vec3 p) {
   return (centerPos - p) / centerScale;
 }
 
-float stepLen = 1.0;
-float okdist = 0.2;
-int maxSteps = 1000;
+float stepLen = 0.01;
+float okdist = 0.5;
+int maxSteps = 100;
+vec4 gcol(float x) { return vec4(vec3(x), 1.0); }
+
+//float cm = max(max(cd.z, cd.y), cd.x);
 vec4 start_march(vec3 spos, vec3 dir) {
   vec3 cd = dist(spos);
-  float cm = max(max(cd.z, cd.y), cd.x);
+  float cm = length(cd);
   int cstep = 0;
 
   while (cm > okdist && cstep < maxSteps) {
     ++cstep;
     cd += dir * stepLen;
-    vec3 acd = abs(cd);
-    cm = max(max(acd.z, acd.y), acd.x);
+    cm = length(cd);
     if (cm < okdist) {
-      return vec4(1.0);
+      return vec4(dir, 1.0);
     }
   }
   if (cstep == maxSteps) {
-    return vec4(0.0);
+    return vec4(vec3(0.1), 0.2);
   } else {
-    return vec4(1.0);
+    return vec4(dir, 1.0);
   }
   //float cm = max(max(cd.x, cd.y), cd.z);
   return vec4(vec3(cstep / maxSteps), 0.6);
@@ -49,7 +49,7 @@ vec4 start_march(vec3 spos, vec3 dir) {
 void main() {
   float px = abs(pp.x - camPos.x);
   vec3 dist = camPos - pp;
-  float latDist = sqrt(p2(dist.x) + p2(dist.z));
+  float latDist = length(dist.xz);
   float vertAngle = atan(-dist.y / latDist);
   int quad;
   if (dist.z >= 0) { if (dist.x >= 0) { quad = 3; } else { quad = 2; } } 
